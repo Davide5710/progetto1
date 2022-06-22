534492c9979436b7490f3227400ba43e7f4501b4
Davide5710 <davide.the.strong@gmail.com>
2022-06-22 10:07:34 +0200

    modificaTesto2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ciao mondo</Text>
+      <Text>Come va?</Text>
       <StatusBar style="auto" />
     </View>
   );
